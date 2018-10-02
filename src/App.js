272a81ab3032f03bb443aaa2c60973b72b464d7b
James Albert Phillips V <jamesalbertphillipsv@jamess-air.attlocal.net>
2018-10-01 22:51:29 -0700

    added cards, needs more functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+// import logo from './logo.svg';
 import './App.css';
 
-let images = [
-  {
-    id: 1,
-    photo: logo,
-    hasBeenClicked: false
-  },
-  {
-    id: 2,
-    photo: logo,
-    hasBeenClicked: false
-  },
-  {
-    id: 3,
-    photo: logo,
-    hasBeenClicked: false
-  },
-  {
-    id: 4,
-    photo: logo,
-    hasBeenClicked: false
-  },
-]
+// let images = [
+//   {
+//     id: 1,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 2,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 3,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 4,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 5,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 6,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 7,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 8,
+//     photo: logo,
+//     hasBeenClicked: false
+//   },
+//   {
+//     id: 9,
+//     photo: logo,
+//     hasBeenClicked: false
+//   }
+// ]
 
 class App extends Component {
 
@@ -31,28 +60,28 @@ class App extends Component {
     super()
     this.state = {
       score: 0,
-      images: images
+      friends
     }
   }
 
   // this one is mine
-  handleImageClick = (id) => {
-    console.log(id);
+  // handleImageClick = (id) => {
+  //   console.log(id);
 
-    for (var i = 0; i < images.length; i++) {
-      if(images[i].id === id){
+  //   for (var i = 0; i < images.length; i++) {
+  //     if(images[i].id === id){
 
-        if(images[i].hasBeenClicked === false){
-          images[i].hasBeenClicked = true
-        } else {
-          alert('you loose! becuase that has already been clicked')
-        }
-      }
-    }
-    this.setState({
-      score: this.state.score + 1
-    })
-  }
+  //       if(images[i].hasBeenClicked === false){
+  //         images[i].hasBeenClicked = true
+  //       } else {
+  //         alert('you lose! becuase that has already been clicked')
+  //       }
+  //     }
+  //   }
+  //   this.setState({
+  //     score: this.state.score + 1
+  //   })
+  // }
 
   // 1 of 11 react lifecycle method
   componentDidMount(){
@@ -65,23 +94,25 @@ class App extends Component {
 
   render() {
 
-    const displayImages = this.state.images.map((eachItem, index) =>
-      <img onClick={() => this.handleImageClick(eachItem.id)} src={eachItem.photo} alt="hello" key={index}/>
-    )
+    // const displayImages = this.state.images.map((eachItem, index) =>
+    //   <img onClick={() => this.handleImageClick(eachItem.id)} src={eachItem.photo} alt="hello" key={index}/>
+    // )
 
     return (
-      <div>
-        <div className="App">
-          <h1>Clicky Game</h1>
-
-        </div>
-
-        <p>Score is {this.state.score}</p>
-        <button onClick={this.handleImageClick}>Image</button>
-        <div className="images">
-          {displayImages}
-        </div>
-      </div>
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
